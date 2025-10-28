7dc189806bb2c9fb5385a3562e9870cced9ee229
Andrew ONeill <andy@evolve757.com>
2025-10-28 10:32:09 -0400

    finished the second exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace PalindromeExercise;
+
+public class WordSmith
+{
+    public bool IsPalindrome(string word)
+    {
+        var wordReversed = "";
+        for (int i = word.Length - 1; i >= 0; i--)
+        {
+            wordReversed += word[i];
+        }
+
+        if (wordReversed == word)
+        {
+            return true;
+        }
+        return false;
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,23 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("radar", true)]
+        [InlineData("kayak", true)]
+        [InlineData("false", false)]
+        [InlineData("together", false)]
+        
+        public void Test1(string word, bool expected)
         {
-
+            var tester = new WordSmith();
+            var actual = tester.IsPalindrome(word);
+            Assert.Equal(expected, actual);
         }
     }
 }
